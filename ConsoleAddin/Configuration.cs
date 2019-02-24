bd8faad14ea988a32964eeab2557acdb6584a183
Rick Strahl <rstrahl@west-wind.com>
2019-02-24 00:02:14 -1000

    Add project files.

---
+++ ConsoleAddin/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using MarkdownMonster.AddIns;
+
+namespace ConsoleAddin
+{
+    public class ConsoleAddinConfiguration : BaseAddinConfiguration<ConsoleAddinConfiguration>
+    {
+        public ConsoleAddinConfiguration()
+        {
+            // uses this file for storing settings in `%appdata%\Markdown Monster`
+            // to persist settings call `ConsoleAddinConfiguration.Current.Write()`
+            // at any time or when the addin is shut down
+            ConfigurationFilename = "ConsoleAddin.json";
+        }
+
+        // Add properties for any configuration setting you want to persist and reload
+        // you can access this object as 
+        //     ConsoleAddinConfiguration.Current.PropertyName
+    }
+}
